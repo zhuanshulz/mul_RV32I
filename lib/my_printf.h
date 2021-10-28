c2cf4fb359b9a9b458af791461842f93b986d369
zhuanshulz <yangling404690228@qq.com>
2021-10-27 20:27:36 -0700

    udpate myprintf lib

---
+++ lib/my_printf.h
@@ -0,0 +1,74 @@
+#include <stdarg.h>
+
+void printf_num(unsigned int num);
+void printf_deci(int dec);
+void printf_char(int ch);
+void printf_str(char* str);
+void my_printf(char* str,...);
+
+volatile char* outputAddr = (char*)0x7f030000;
+
+void printf_num(unsigned int num){
+	if(num!=0){
+		printf_num(num/10);
+		unsigned int num_char=num%10+48;
+		*outputAddr=(char)num_char;
+		return;
+	}
+	else{
+		return;
+	}
+	 
+}
+
+void printf_deci(int dec){
+	if(dec==0){
+		*outputAddr='0';
+		return;
+	}
+	if(dec<0){
+		*outputAddr='-';
+		dec=0 - dec;
+	}
+	printf_num(dec);
+}
+
+void printf_char(int ch){
+	*outputAddr=(char)ch;
+}
+
+void printf_str(char* str){
+	int i=0;
+	for(i=0;str[i]!='\0';i++){
+		*outputAddr=str[i];
+	}
+}
+
+void my_printf(char* str, ...){
+	va_list va_ptr;
+	va_start(va_ptr,str);
+	int i;
+	for(i=0;str[i]!='\0';i++){
+		if(str[i]!='%'){
+			*outputAddr=str[i];
+			continue;
+		}
+		switch(str[++i]){
+			case 'd':
+				printf_deci(va_arg(va_ptr,int));	
+				break;
+			case 'c':
+				printf_char(va_arg(va_ptr,int));
+				break;
+			case 's':
+				printf_str(va_arg(va_ptr,char*));
+				break;
+			default:
+				break;
+
+		}
+	}
+	va_end(va_ptr);
+}
+
+
